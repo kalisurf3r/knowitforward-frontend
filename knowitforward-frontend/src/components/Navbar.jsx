da1d62acf303f9975baa69675dd9eb1cdca3f422
kalisurf3r <ayalaarturo925@gmail.com>
2024-08-29 12:07:47 -0700

    footer test

---
+++ knowitforward-frontend/src/components/Navbar.jsx
@@ -0,0 +1,72 @@
+import {Link, useLocation} from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+
+export default function Navbar() {
+    const logoStyle = {
+        height: '50px',
+        width: 'auto',
+        marginRight: '10px'
+    }
+
+    const navbarStyle = {
+        backgroundColor: '#F0F0F0',
+        fontFamily: 'Rubik, sans-serif'
+    }
+
+    const navitemStyle = {
+        color: 'black',
+        textDecoration: 'none'
+    }
+
+    const usernameStyle = {
+        color: 'black',
+        opacity: 0.7, // Set the desired opacity value here
+       marginLeft: '100px'
+    };
+
+
+    const [username, setUsername] = useState('JohnDoe');
+
+    // useEffect(() => {
+    //     const fetchUsername = async () => {
+    //         const user = await getUserFromAuthService();
+    //         if (user) {
+    //             setUsername(user.username);
+    //         }
+    //     };
+    //     fetchUsername();
+    // }, []);
+
+
+    return (
+        <nav className="navbar navbar-expand-lg" style={navbarStyle}>
+        <div className="container-fluid">
+            <div className="navbar-brand">
+                <img src="logo.png" alt="logo" style={logoStyle} />
+            </div>
+            <span className="nav-link" style={usernameStyle}>Hi, {username}</span>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav ms-auto">
+                    <li className="nav-item ms-4">
+                        <Link to="/profile" className="nav-link" style={navitemStyle}>Profile</Link>
+                    </li>
+                    <li className="nav-item ms-4">
+                        <Link to="/volunteer" className="nav-link" style={navitemStyle}>Volunteer</Link>
+                    </li>
+                    <li className="nav-item ms-4">
+                        <Link to="/services" className="nav-link" style={navitemStyle}>Services</Link>
+                    </li>
+                    <li className="nav-item ms-4">
+                        <Link to="/charity" className="nav-link" style={navitemStyle}>Charity</Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    
+    )
+}
--- knowitforward-frontend/src/main.jsx
+++ knowitforward-frontend/src/main.jsx
@@ -1,6 +1,32 @@
 
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import ReactDom from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+const router = createBrowserRouter([
+  {
+      path: '/',
+      element: <App />,
+      children: [
+          {
+             
+          },           
+          {
+             
+          },
+          {
+             
+          },
+          {
+              
+          },
+      ],
+  },
+]);
 
-createRoot(document.getElementById('root')).render(<App />)
+ReactDom.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+)
