98b19cb9ef7b97340b2b7553be164951469028f0
kalisurf3r <ayalaarturo925@gmail.com>
2024-08-30 12:49:59 -0700

    charity card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
 import Signup from "./components/Signup"
+import CharityCard from "./components/CharityCard"
 import './App.css'
 
 function App() {
   
   return (
     <>
-     <Signup />
+     <CharityCard />
     </>
   )
 }
