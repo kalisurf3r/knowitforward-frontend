508ed6ae65fbfcc29a89291bbc42b2a5c2d4346e
kalisurf3r <ayalaarturo925@gmail.com>
2024-09-04 11:44:38 -0700

    added responsive and new redirect link

--- src/pages/Charities.jsx
+++ src/pages/Charities.jsx
@@ -1,7 +1,53 @@
 import "./charities.css";
 import CharityCard from "../components/CharityCard";
+import { getCharities } from "../utils/apiUtil";
+import { useEffect, useState } from "react";
 
-export default function Charities() {
+export default function Charities(props) {
+
+  const [charities, setCharities] = useState([]);
+  
+  const charityFetch = async () => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      console.error("No JWT token found");
+      return;
+    }
+
+    try {
+        
+        const response = await getCharities();
+        
+        if (response.status === 200) {
+            // const data = await response.json();
+            console.log("Fetched data:", response);
+
+            if (response) {
+                const mappedData = response.data.map((charity) => {
+                    return {
+                        id: charity.id,
+                        charityName: charity.charityName,
+                        charityDesc: charity.charityDesc,
+                        wallet: charity.wallet,
+                        logoImgUrl: charity.logoImgUrl,
+                        websiteUrl: charity.websiteUrl,
+                    };
+                });
+                setCharities(mappedData);
+            } else {
+                console.error("Expected an array but got:", data);
+            }
+        } else {
+            console.error("Failed to fetch charities");
+        }
+    } catch (error) {
+        console.error("Error fetching charities:", error);
+    }
+  };
+
+  useEffect(() => {
+    charityFetch();
+  }, []);
 
 
   return (
@@ -19,25 +65,12 @@ export default function Charities() {
         </div>
       </div>
 
-      <div className="row" style={{ marginTop: "85px" }} >
-        <div className="col-md-4 mb-3">
-          <CharityCard />
-        </div>
-        <div className="col-md-4 mb-3">
-          <CharityCard />
-        </div>
-        <div className="col-md-4 mb-3">
-          <CharityCard />
-        </div>
-        <div className="col-md-4 mb-3">
-          <CharityCard />
-        </div>
-        <div className="col-md-4 mb-3">
-          <CharityCard />
-        </div>
-        <div className="col-md-4 mb-3">
-          <CharityCard />
-        </div>
+        <div className="row" style={{ marginTop: "85px" }}>
+        {charities.map((charity) => (
+          <div className="col-12 col-md-6 col-lg-4  col-sm-4 mb-4" key={charity.id}>
+            <CharityCard charity={charity}/>
+          </div>
+        ))}
       </div>
     </>
   );
